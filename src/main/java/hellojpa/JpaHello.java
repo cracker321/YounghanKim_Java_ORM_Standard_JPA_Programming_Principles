9886453093da51c3ead584cfcb44024bb27ea3c0
yujong <yujong4170@gmail.com>
2023-02-09 19:29:33 +0900

    yujong

--- src/main/java/hellojpa/JpaHello.java
+++ src/main/java/hellojpa/JpaHello.java
@@ -11,7 +11,12 @@ import javax.persistence.Persistence;
 @SpringBootApplication
 public class JpaHello {
     public static void main(String[] args){
-        //df
+        /*
+        QMember m = QMember.member;
+
+
+
+         */
        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
        EntityManager em = emf.createEntityManager();
